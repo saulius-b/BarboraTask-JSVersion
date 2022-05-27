abd16a77d491240ef92e1e573211e4002509aa26
Saulius <sauliunas.balciunas@gmail.com>
2022-05-27 21:01:52 +0300

    Final version - the function now returns reports for all the customers

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-import customerList from './data/customers.json' assert {type: "json"};
-import productList from './data/products.json' assert {type: "json"};
-import receiptsList from './data/receipts.json' assert {type: "json"};
+import customers from './data/customers.json' assert {type: "json"};
+import products from './data/products.json' assert {type: "json"};
+import receipts from './data/receipts.json' assert {type: "json"};
 
-const generateCustomerReport = (customerId) => {
+const generateCustomerReport = () => {
 
-	//Getting the customer receipt data with customerId
-	const customerReceipts = receiptsList.filter(item => item.customerId === customerId);
+	const result = []
 
-	//Getting the customer data with customerId
-	const customer = customerList.find(item => item.id === customerId);
+	customers.forEach(element => {
+		//Customer information object
+		const customer = { id: element.id, firstName: element.firstName, lastName: element.lastName }
 
-	//Getting the customer item objects and sorting them by date
-	const items = []
+		//Getting the customer receipt data with customerId
+		const customerReceipts = receipts.filter(item => item.customerId === element.id);
 
-	for (const element of customerReceipts) {
+		//Getting the customer item objects and sorting them by date
+		const items = []
 
-		const customerProduct = productList.filter(item => item.id === element.productId)
+		for (const element of customerReceipts) {
 
-		items.push({ ...element, ...customerProduct[0] })
-	}
+			const customerProduct = products.filter(item => item.id === element.productId)
 
-	items.sort((a, b) => {
+			items.push({ ...element, ...customerProduct[0] })
+		}
 
-		const dateA = new Date(a.createdAt);
-		const dateB = new Date(b.createdAt);
+		items.sort((x, y) => +new Date(x.createdAt) - +new Date(y.createdAt));
 
-		return dateA - dateB;
+		//Getting total price
+		const total = items.reduce((accum, item) => accum + (parseInt(item.price) * item.quantity), 0)
+
+		//Getting byDay object
+		const dates = items.map(item => item.createdAt.split('T')[0])
+
+		const prices = items.map(item => parseInt(item.price) * item.quantity)
+
+		const byDay = {}
+
+		dates.forEach((item, index) => {
+
+			byDay[item] = prices[index]
+
+		});
+
+		//End result
+		result.push(
+			{
+				[element.id]: {
+					customer,
+					items,
+					total,
+					byDay
+				}
+			}
+		)
 	});
-
-	//Getting total price
-	const total = items.reduce((accum, item) => accum + (parseInt(item.price) * item.quantity), 0)
-
-	//Getting byDay object
-	const dates = items.map(item => item.createdAt.split('T')[0])
-
-	const prices = items.map(item => parseInt(item.price) * item.quantity)
-
-	const byDay = {}
-
-	dates.forEach((item, index) => {
-
-		byDay[item] = prices[index]
-
-	});
-
-	//End result
-	const result = {
-		customer, items, total, byDay,
-	}
 
 	return result
-
 }
 
-generateCustomerReport('85c9427f-950f-4936-bcd5-3ed9823c9900')
+console.log(generateCustomerReport())
